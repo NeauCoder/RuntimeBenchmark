a19cf43ae182605764cf464cfaf79a63705c7ed7
codelover <386273319@qq.com>
2019-06-21 17:38:59 +0800

    添加项目文件。

---
+++ RuntimeBenchmark/Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿
+using CefSharp;
+using CefSharp.WinForms;
+namespace RuntimeBenchmark
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            //this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.webBrowser1 = new ChromiumWebBrowser("https://www.baidu.com",null);
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(107, 12);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(975, 474);
+            this.webBrowser1.TabIndex = 0;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1226, 610);
+            this.Controls.Add(this.webBrowser1);
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "RuntimeBenchmark";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        //private System.Windows.Forms.WebBrowser webBrowser1;
+        private ChromiumWebBrowser webBrowser1;
+    }
+}
+
